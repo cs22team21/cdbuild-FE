a1de367a55aa97c267c9abad1a3a6c401619b0bc
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-10-25 09:20:41 -0500

    added game style

--- mud-client/src/components/Game/Game.js
+++ mud-client/src/components/Game/Game.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-import Controls from './gameControls';
-import Map from './Map'
+import Controls from "./gameControls";
+import Map from "./Map";
+import "./Game.scss";
 // import Player from './Player';
 
 const Game = props => {
@@ -19,7 +20,7 @@ const Game = props => {
     console.log(options);
     axios.get(`${props.backendUrl}/api/adv/position/`, options).then(res => {
       setUser(res.data);
-      console.log('resData', res.data)
+      console.log("resData", res.data);
     });
   }, [props.logIn, props.backendUrl]);
 
@@ -36,23 +37,30 @@ const Game = props => {
       .post(`${props.backendUrl}/api/adv/move/`, { direction }, options)
       .then(res => {
         setUser(res.data);
-        console.log(res.data)
+        console.log(res.data);
       });
   }
 
-  console.log('user', user);
+  console.log("user", user);
 
   return (
     <React.Fragment>
-      <div className="game-map">
-        <Map logIn={props.logIn} backendUrl={props.backendUrl} user={user} />
-      </div>
-      <div className="game-player">
-        <div>
-          <h3>User:  {user.name}</h3>
-          <h4>Current Room:  {user.error_msg === "" ? user.title : `${user.error_msg} you are still in the ${user.title}`}</h4>
+      <div className="game-wrapper">
+        <div className="game-map">
+          <Map logIn={props.logIn} backendUrl={props.backendUrl} user={user} />
         </div>
-        <Controls moveDirection={moveDirection} />
+        <div className="game-player">
+          <div>
+            <h3>User: {user.name}</h3>
+            <h4>
+              Current Room:{" "}
+              {user.error_msg === ""
+                ? user.title
+                : `${user.error_msg} you are still in the ${user.title}`}
+            </h4>
+          </div>
+          <Controls moveDirection={moveDirection} />
+        </div>
       </div>
     </React.Fragment>
   );
